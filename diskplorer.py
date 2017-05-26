f27d41edc45a1f2dc6ce84c37f612c94cfa015d4
Vlad Zolotarov <vladz@scylladb.com>
2017-05-26 12:16:58 -0400

    Add the ability to pass the test name
    A test name may be one of 'read', 'write', 'randread', 'randwrite'.
    
    Signed-off-by: Vlad Zolotarov <vladz@scylladb.com>

--- diskplorer.py
+++ diskplorer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 from __future__ import print_function
 
@@ -9,6 +9,7 @@ import matplotlib
 import math
 import optparse
 import multiprocessing
+import re
 
 matplotlib.use('svg')  # must come before pyplot import
 
@@ -18,6 +19,8 @@ optparser = optparse.OptionParser()
 
 optparser.add_option('-d', '--mountpoint', dest='mountpoint', default='.',
                      help='Test disk mounted at MOUNTPOINT', metavar='MOUNTPOINT')
+optparser.add_option('-t', '--test', dest='test_name', default='randread', choices=['read', 'write', 'randwrite', 'randread'],
+                     help='Test to run', metavar='TEST')
 optparser.add_option('-b', '--device', dest='device', default=None,
                      help='Test block device DEV (overrides --mountpoint)', metavar='DEV')
 optparser.add_option('-s', '--filesize', dest='filesize', default='100G',
@@ -40,8 +43,14 @@ maxdepth = options.maxdepth
 buffer_size = options.buffer_size
 output_filename = options.output_filename
 raw_filename = options.raw_results_filename
+test_name = options.test_name
 input_filename = 'fiotest.tmp'
 readonly = []
+stat_label = 'read'
+
+if re.search('write', test_name):
+    stat_label = 'write'
+
 if options.device:
     input_filename = options.device
     readonly = ['--readonly']
@@ -51,7 +60,7 @@ header = '''\
 [global]
 ioengine=libaio
 buffered=0
-rw=randread
+rw={test_name}
 bs={buffer_size}
 size={filesize}
 directory={mountpoint}
@@ -108,11 +117,11 @@ iopses = [0.]
 
 for job in results['jobs']:
     concurrency = int(job['jobname'])
-    latency = float(job['read']['clat']['mean'])
-    latency_05 = float(job['read']['clat']['percentile']['5.000000'])
-    latency_95 = float(job['read']['clat']['percentile']['95.000000'])
-    latency_stddev = float(job['read']['clat']['stddev'])
-    iops = float(job['read']['iops'])
+    latency = float(job[stat_label]['clat']['mean'])
+    latency_05 = float(job[stat_label]['clat']['percentile']['5.000000'])
+    latency_95 = float(job[stat_label]['clat']['percentile']['95.000000'])
+    latency_stddev = float(job[stat_label]['clat']['stddev'])
+    iops = float(job[stat_label]['iops'])
     concurrencies.append(concurrency)
     latencies.append(latency)
     latencies_05.append(latency_05)
@@ -126,18 +135,18 @@ fig, ax1 = plt.subplots()
 ax1.plot(concurrencies, iopses, 'b-+')
 ax1.set_xlabel('concurrency')
 # Make the y-axis label and tick labels match the line color.
-ax1.set_ylabel('{buffer_size} read iops'.format(**globals()), color='b')
+ax1.set_ylabel('{buffer_size} {stat_label} iops'.format(**globals()), color='b')
 for tl in ax1.get_yticklabels():
     tl.set_color('b')
 # FIXME: want log scale on X axis
-    
+
 ax2 = ax1.twinx()
 #ax2.plot(concurrencies, latencies, 'r-+')
 ax2.errorbar(concurrencies, latencies, yerr=[latencies_05, latencies_95], color='r')
 ax2.set_ylabel(u'average latency (us)', color='r')
 for tl in ax2.get_yticklabels():
     tl.set_color('r')
-    
+
 plt.savefig(filename=output_filename)
 
 with open(raw_filename, 'w') as raw:
