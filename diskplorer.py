8d2581769f28bbcf71129d7d2f2e4e7080b2f18b
Vlad Zolotarov <vladz@scylladb.com>
2020-11-18 21:56:46 -0500

    diskplorer.py: test config from the json file when the result file is provided
    Read the test type, the buffer size and the fsync configuration from the
    JSON result file when it's provided.
    
    Signed-off-by: Vlad Zolotarov <vladz@scylladb.com>

--- diskplorer.py
+++ diskplorer.py
@@ -10,82 +10,49 @@ import math
 import optparse
 import multiprocessing
 import re
+import sys
 
 matplotlib.use('svg')  # must come before pyplot import
 
 import matplotlib.pyplot as plt
 
-optparser = optparse.OptionParser()
+def set_output_files_names(buffer_size, test_name, output_filename_prefix, fsync_io):
+    """
+    Returns a tuple of strings that represent names of output SVG, CSV and JSON files correspondingly.
 
-optparser.add_option('-d', '--mountpoint', dest='mountpoint', default='.',
-                     help='Test disk mounted at MOUNTPOINT', metavar='MOUNTPOINT')
-optparser.add_option('-t', '--test', dest='test_name', default='randread', choices=['read', 'write', 'randwrite', 'randread'],
-                     help='Test to run', metavar='TEST')
-optparser.add_option('-b', '--device', dest='device', default=None,
-                     help='Test block device DEV (overrides --mountpoint)', metavar='DEV')
-optparser.add_option('-s', '--filesize', dest='filesize', default='100G',
-                     help='Set SIZE as file size for test', metavar='SIZE')
-optparser.add_option('--buffer-size', dest='buffer_size', default='4k',
-                     help='Set SIZE as I/O buffer size for test (ex. 4k, 1M)', metavar='SIZE')
-optparser.add_option('-m', '--max-concurrency', dest='maxdepth', default=128, type='int',
-                     help='Test maximum concurrency level N', metavar='N')
-optparser.add_option('-o', '--output-prefix', dest='output_filename_prefix', default='disk-concurrency-response',
-                     help='Write output graph and csv to FILE prefixed with this', metavar='FILE')
-optparser.add_option('-f','--file', dest='fio_json',
-                     metavar='FILE',
-                     help='Input file with fio results for processing')
-optparser.add_option('--fsync-io', dest='fsync_io', default=0, type='int', metavar='IOP',
-                     help="How many I/Os to perform before issuing an fsync(2) of dirty data. If 0, don't sync. Default: 0.")
+    :param buffer_size: buffer size used for the test
+    :param test_name: fio test name
+    :param output_filename_prefix: user provided output file prefix
+    """
+    output_filename = output_filename_prefix+"-{buffer_size}-{test_name}-fsync-{fsync_io}.svg".format(buffer_size=buffer_size,test_name=test_name, fsync_io=fsync_io)
+    raw_filename = output_filename_prefix+"-{buffer_size}-{test_name}-fsync-{fsync_io}.csv".format(buffer_size=buffer_size,test_name=test_name, fsync_io=fsync_io)
+    json_filename = output_filename_prefix+'-{buffer_size}-{test_name}-fsync-{fsync_io}.fio.json'.format(buffer_size=buffer_size,test_name=test_name, fsync_io=fsync_io)
 
-
-(options, args) = optparser.parse_args()
-
-mountpoint = options.mountpoint
-filesize = options.filesize
-maxdepth = options.maxdepth
-buffer_size = options.buffer_size
-test_name = options.test_name
-output_filename = options.output_filename_prefix+"-{buffer_size}-{test_name}.svg".format(buffer_size=buffer_size,test_name=test_name)
-raw_filename = options.output_filename_prefix+"-{buffer_size}-{test_name}.csv".format(buffer_size=buffer_size,test_name=test_name)
-json_filename = options.output_filename_prefix+'-{buffer_size}-{test_name}.fio.json'.format(buffer_size=buffer_size,test_name=test_name)
-fio_input_filename = options.output_filename_prefix+'-fiotest.tmp'
-readonly = []
-stat_label = 'read'
-fsync_io = options.fsync_io
-
-if re.search('write', test_name):
-    stat_label = 'write'
-
-if options.device:
-    fio_input_filename = options.device
-    readonly = ['--readonly']
-    mountpoint = '/'
-
-header = '''\
-[global]
-ioengine=libaio
-buffered=0
-rw={test_name}
-bs={buffer_size}
-size={filesize}
-directory={mountpoint}
-runtime=10s
-filename={fio_input_filename}
-group_reporting=1
-fsync={fsync_io}
-
-'''
-
-job_template = '''\
-[{jobname}]
-iodepth={depth}
-{new_group}
-
-'''
-
-max_threads = multiprocessing.cpu_count()
+    return output_filename, raw_filename, json_filename
 
 def create_fio_spec(fname):
+    header = '''\
+    [global]
+    ioengine=libaio
+    buffered=0
+    rw={test_name}
+    bs={buffer_size}
+    size={filesize}
+    directory={mountpoint}
+    runtime=10s
+    filename={fio_input_filename}
+    group_reporting=1
+    fsync={fsync_io}
+
+    '''
+
+    job_template = '''\
+    [{jobname}]
+    iodepth={depth}
+    {new_group}
+
+    '''
+
     with open(fname, 'w') as f:
         f.write(header.format(**globals()))
         depth = 1
@@ -112,13 +79,88 @@ def run_job():
     open(json_filename, 'w').write(result_json)
     return json.loads(result_json)
 
+tests_choices = ['read', 'write', 'randwrite', 'randread']
+
+optparser = optparse.OptionParser()
+
+optparser.add_option('-d', '--mountpoint', dest='mountpoint', default='.',
+                     help='Test disk mounted at MOUNTPOINT', metavar='MOUNTPOINT')
+optparser.add_option('-t', '--test', dest='test_name', choices=tests_choices,
+                     help='Test to run', metavar='TEST')
+optparser.add_option('-b', '--device', dest='device', default=None,
+                     help='Test block device DEV (overrides --mountpoint)', metavar='DEV')
+optparser.add_option('-s', '--filesize', dest='filesize', default='100G',
+                     help='Set SIZE as file size for test', metavar='SIZE')
+optparser.add_option('--buffer-size', dest='buffer_size', default='4k',
+                     help='Set SIZE as I/O buffer size for test (ex. 4k, 1M)', metavar='SIZE')
+optparser.add_option('-m', '--max-concurrency', dest='maxdepth', default=128, type='int',
+                     help='Test maximum concurrency level N', metavar='N')
+optparser.add_option('-o', '--output-prefix', dest='output_filename_prefix', default='disk-concurrency-response',
+                     help='Write output graph and csv to FILE prefixed with this', metavar='FILE')
+optparser.add_option('-f','--file', dest='fio_json',
+                     metavar='FILE',
+                     help='Input file with fio results for processing')
+optparser.add_option('--fsync-io', dest='fsync_io', default=0, type='int', metavar='IOP',
+                     help="How many I/Os to perform before issuing an fsync(2) of dirty data. If 0, don't sync. Default: 0.")
+
+########################################################################################################################
+(options, args) = optparser.parse_args()
+
+mountpoint = options.mountpoint
+filesize = options.filesize
+maxdepth = options.maxdepth
+buffer_size = options.buffer_size
+fsync_io = options.fsync_io
+
+if not options.test_name and not options.fio_json:
+    sys.exit("Test name is not set - choose one: {}".format(tests_choices))
+
+test_name = options.test_name
+output_filename, raw_filename, json_filename = set_output_files_names(buffer_size, test_name, options.output_filename_prefix, fsync_io)
+
+fio_input_filename = options.output_filename_prefix+'-fiotest.tmp'
+readonly = []
+
+if options.device:
+    fio_input_filename = options.device
+    readonly = ['--readonly']
+    mountpoint = '/'
+
+max_threads = multiprocessing.cpu_count()
 results = ""
 
+# Run the test
+
 if options.fio_json:
-  with open(options.fio_json,"r") as json_file:
-    results = json.load(json_file)
+    with open(options.fio_json,"r") as json_file:
+        results = json.load(json_file)
+
+        if 'global options' not in results:
+            sys.exit("Bad format in {}: 'global options' section in not present".format(options.fio_json))
+
+        global_options = results['global options']
+
+        if 'rw' not in global_options:
+            sys.exit("Bad format in {}: 'rw' field is in not present".format(options.fio_json))
+
+        test_name = global_options['rw']
+
+        if 'bs' not in global_options:
+            sys.exit("Bad format in {}: 'bs' field is in not present".format(options.fio_json))
+
+        buffer_size = global_options['bs']
+
+        if 'fsync' not in global_options:
+            fsync_io = 0
+
+        fsync_io = global_options['fsync']
+
+        output_filename, raw_filename, json_filename = set_output_files_names(buffer_size, test_name, options.output_filename_prefix, fsync_io)
+
 else:
-  results = run_job()
+    results = run_job()
+
+# Generate result files
 
 concurrencies = [0]  # FIXME: fake 0 element to force axis limit
 latencies = [0.]
@@ -128,6 +170,10 @@ latencies_99 = [0.]
 latencies_9999 = [0.]
 latencies_max = [0.]
 iopses = [0.]
+
+stat_label = 'read'
+if re.search('write', test_name):
+    stat_label = 'write'
 
 for job in results['jobs']:
     concurrency = int(job['jobname'])
